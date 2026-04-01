983861cf3393142afd2fa03b167c32900c4fb50d
Pilou <76021631+0xPilou@users.noreply.github.com>
2026-04-01 15:28:11 +0200

    chore: revise interface

--- src/interfaces/IYieldStrategy.sol
+++ src/interfaces/IYieldStrategy.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.34;
 /// @notice Interface for the external yield strategy contract.
 ///         The vault operator uses this to deploy and withdraw assets.
 interface IYieldStrategy {
+
     /// @notice Deploys assets from the vault into the yield strategy.
     /// @param assets Amount of underlying assets to deploy
     function deploy(uint256 assets) external;
@@ -19,4 +20,5 @@ interface IYieldStrategy {
     ///      for settlement, not this value directly.
     /// @return totalValue Current value denominated in the vault's underlying asset
     function totalValue() external view returns (uint256 totalValue);
+
 }
