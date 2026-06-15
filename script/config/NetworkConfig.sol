bd40ff4ef98c113750073162ca3ad4e7218275ce
Pilou <76021631+0xPilou@users.noreply.github.com>
2026-06-15 09:23:41 +0200

    feat: added Base config

--- script/config/NetworkConfig.sol
+++ script/config/NetworkConfig.sol
@@ -19,6 +19,8 @@ library NetworkConfig {
     function getNetworkConfig(uint256 chainId) internal pure returns (DeploymentConfig memory config) {
         if (chainId == 137) {
             config = getPolygonMainnetConfig();
+        } else if (chainId == 8453) {
+            config = getBaseMainnetConfig();
         } else {
             revert("Unsupported chainId");
         }
@@ -39,6 +41,24 @@ library NetworkConfig {
             guaranteedFlowDuration: 0,
             shareName: "",
             shareSymbol: ""
+        });
+    }
+
+    /**
+     * @dev Get Base Mainnet configuration
+     */
+    function getBaseMainnetConfig() internal pure returns (DeploymentConfig memory) {
+        return DeploymentConfig({
+            treasury: 0xdc36265ca4505021250F02d3b711Dd9e9F23aD3D,
+            underlyingAsset: 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913,
+            yieldAsset: 0xD04383398dD2426297da660F9CCA3d439AF9ce1b,
+            externalVault: 0xbeef0e0834849aCC03f0089F01f4F1Eeb06873C9,
+            fundOperator: 0xB9337958009Fc5b320844FE34F9eb58D8018837C,
+            fundAdmin: 0xdc36265ca4505021250F02d3b711Dd9e9F23aD3D,
+            initialEraStableYieldRate: 300,
+            guaranteedFlowDuration: 2 days,
+            shareName: "SYV v0 Share",
+            shareSymbol: "SYVV0"
         });
     }
 
